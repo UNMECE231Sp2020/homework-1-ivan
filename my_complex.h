0303c8e095fc3ff9acb124ccb702a296119ccd5b
ivannunez98 <ivan.a.nunez98@gmail.com>
2020-02-06 03:17:55 -0700

    Added two function prtotypes for handlers for main.c to accsess.

--- my_complex.h
+++ my_complex.h
@@ -18,3 +18,7 @@ Complex complex_conj(Complex c);
 
 double magnitude(Complex c);
 double phase(Complex c);
+
+void complex_hdlr(Complex (*fun) (Complex, Complex), Complex c1, Complex c2);
+void double_hdlr(double (*dub_fun)(Complex), Complex c1);
+
