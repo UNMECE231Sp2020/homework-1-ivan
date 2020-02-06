b44ff028e9f2b3aea465203dec4d1da65be912d0
ivannunez98 <ivan.a.nunez98@gmail.com>
2020-02-06 03:12:33 -0700

    Added arrays to send functions add, sub, div, mult ,magnitude, and phase complex numbers and print the result.

--- main.c
+++ main.c
@@ -5,5 +5,18 @@ int main() {
 	Complex a = {3, 4};
 	Complex b = {5, 12};
 
+	Complex (*fun[4])(Complex c1, Complex c2) = {complex_add, complex_sub, complex_mult, complex_div};
+
+	double (*dub_fun[2])(Complex c) = {magnitude, phase};
+
+	int x;
+	for(x=0; x<4; x++){
+		complex_hdlr(fun[x], a, b);
+	}
+
+	for(x=0; x<2; x++){
+		double_hdlr(dub_fun[x], a);
+	}
+
 	return 0;
 }
